26eeb7cd5e90a97aaf2242ab2b93473365b0013d
Praneethpshetty24 <praneeth666s@gmail.com>
2025-04-24 16:34:47 +0530

    pushed

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const fs = require('fs');
+const app = express();
+const port = 3000;
+
+app.get("/data",(req,res)=>{
+    try{
+        const data = fs.readFileSync('./data.json','utf-8');
+        res.json(JSON.parse(data))
+
+    }catch(err){
+        console.log(err);
+        res.status.json({message:"error"})
+    }
+})
+
+app.listen(port,()=>{
+    console.log(`Server is running on port ${port}`);
+})
