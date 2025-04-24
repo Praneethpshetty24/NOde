d6c541a9541fe47ce2f46d0e1df65835dc7b4568
Praneethpshetty24 <praneeth666s@gmail.com>
2025-04-24 16:40:11 +0530

    update

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const port = 3000;
 app.get("/data",(req,res)=>{
     try{
         const data = fs.readFileSync('./data.json','utf-8');
+        res.write("Hello World")
         res.json(JSON.parse(data))
 
     }catch(err){
